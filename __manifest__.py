ef7eb297f2a19c19df59515cf4cd203089a1c9ab
cybro-t6 <adilpk123@gmail.com>
2024-07-05 09:31:22 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Product Brand',
+    'version': '17.0.1.0.0',
+    'summary': 'Brand Products',
+    'author': 'Adil P k',
+    'depends': ['point_of_sale','product'],
+    'description': 'Add a product Brand names in POS order line',
+    'installable': True,
+    'application': True
+}
